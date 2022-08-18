0efd2dc1e7b57b75ae5897d632b2fc3fe77d7739
Maks <kiva_hill@mail.ru>
2022-08-18 20:49:07 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
+
+gulp.task('html', function(){
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('fonts', function(){
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function(){
+    return gulp.src('src/icons/**/*')
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('images', function(){
+    return gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+}); 
+
+
+
+gulp.task('default', gulp.parallel('watch', 'html', 'server', 'styles', 'fonts', 'icons', 'images'));
