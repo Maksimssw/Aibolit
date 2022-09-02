5e03110e8497a7ec04270131c6f99b7a3b165fa1
Maks <kiva_hill@mail.ru>
2022-09-02 21:32:28 +0300

    adaptation

--- src/js/modules/Sliders/sliderSpecialists.js
+++ src/js/modules/Sliders/sliderSpecialists.js
@@ -12,11 +12,29 @@ function sliderSpecialists(item){
     let numberMultiplication;
     let margins;
 
-    if(container.offsetWidth > 1024){
+    if(container.offsetWidth > 1288){
         numberMultiplication = 3;
         sliderItem.forEach(el => {
             widthItem = slider.offsetWidth / 6
             el.style.width = Math.trunc(widthItem) + 'px';
+        });
+        margins = 15 * sliderItem.length - 1;
+        sliderWrapper.style.width = Math.trunc(widthItem) * sliderItem.length + 'px';
+    } else if(container.offsetWidth <= 1288 && container.offsetWidth >= 687){
+        numberMultiplication = 3;
+        sliderItem.forEach(el => {
+            widthItem = slider.offsetWidth / 3
+            el.style.width = Math.trunc(widthItem) + 'px';
+
+        });
+        margins = 15 * sliderItem.length - 1;
+        sliderWrapper.style.width = Math.trunc(widthItem) * sliderItem.length + 'px';
+    } else {
+        numberMultiplication = 1.5;
+        sliderItem.forEach(el => {
+            widthItem = slider.offsetWidth / 1
+            el.style.width = Math.trunc(widthItem) + 'px';
+
         });
         margins = 15 * sliderItem.length - 1;
         sliderWrapper.style.width = Math.trunc(widthItem) * sliderItem.length + 'px';
